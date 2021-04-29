d80e9bcdde3bdc4177a5c3fa2cc2504477ca840d
Carlos A <carlosmiranda19122@gmail.com>
2021-04-29 17:51:31 -0300

    using SSG (Static Site Generation)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,23 @@
-/** Utilizando o modelo SSR (Server side rendering) */
+/** Utilizando o modelo SSG (Static site generator) */
 export default function Home(props) {
-  /**
-   * Ao tentar exibir um console.log no navegador com o método de SSR, ele
-   * não me retorna nada. Isso porque, como o arquivo está sendo executado no
-   * servidor NEXT, o console.log será exibido no segundo servidor (NEXT server)
-   * e não no browser.
-   */
-  // console.log(props.episodes);
-
   return (
     <>
       <h1>Index</h1>
-
-      {/** Para isto acontecer, basta colocar o seguinte código. O servidor
-       * NEXT carregará as informações e entregará para o client um "arquivo
-       * estático com as informações"
-       */}
       <p>{JSON.stringify(props.episodes)}</p>
     </>
   );
 }
 
 /**
- * Apenas por exportar uma função com este nome, o NEXT entenderá que precisa
- * executar esta função antes da renderização acima
+ * Este método serve para salvar uma criação estática da página em questão e
+ * salvá-la em cache.
+ *
+ * O atributo 'revalidate' dita a cada quanto tempo em segundos a página será
+ * atualizada.
+ *
+ * NOTA: Este modelo só funciona em PRODUÇÃO (quando o servidor está "upado")
  */
-export async function getServerSideProps() {
+export async function getStaticProps() {
   const response = await fetch("http://localhost:3333/episodes");
   const data = await response.json();
 
@@ -33,5 +25,6 @@ export async function getServerSideProps() {
     props: {
       episodes: data,
     },
+    revalidate: 60 * 60 * 8, // A cada 8hrs
   };
 }
